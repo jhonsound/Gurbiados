90893fb17caac511fcb71fadfa19f1b95ae63b88
jhonsound <jhoncabaal@gmail.com>
2025-08-04 11:56:37 -0500

    project reset and nativewind configuration

---
+++ components/home.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Text } from "react-native";
+
+const Home = () => {
+  return (
+    <Text className="bg-blue-500 text-red-700 p-5 rounded-md text-2xl">
+      hola mundo...
+    </Text>
+  );
+};
+
+export default Home;
